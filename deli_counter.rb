a0ae4844febf5ba679eb6bf6153e53c565e7e9ef
Andrew Capp <acapp909@gmail.com>
2019-10-11 23:25:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+katz_deli = []
+counter = 0
+string = ""
+
+def line(array)
+  if array.size == 0
+    puts "The line is currently empty."
+  else
+  
+    while counter < array.size do
+      string << "#{counter + 1}.#{array[counter]}"
+      counter += 1
+    end
+    
+    end
+    puts "The line is currently: #{string}"
+end
+
+def take_a_number(current_line_array, person_name)
+
+end
+
+def now_serving(array)
+
+end
+
